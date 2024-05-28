b49da53551a9faade70f9a54286e2dc61011e6db
gerardalessandrov <gerardvigorodrigues@gmail.com>
2024-05-28 01:04:11 -0500

    changes

--- scripts/form.js
+++ scripts/form.js
@@ -1,27 +1,23 @@
 const products = [
     {
-        id: fc - 1888,
-        name: "flux capacitor",
+        id: name - 1,
+        value: "carrots",
         avgeragerating: 4.5
     },
     {
-        id: fc - 2050,
-        name: "power laces",
+        id: fourstar,
+        value: "rice",
         averagerating: 4.7
     },
     {
-        id: fs - 1987,
-        name: "time circuits",
+        id: threestar,
+        value: "chicken",
         averagerating: 3.5
     },
     {
-        id: ac - 2000,
-        name: "low voltage reactor",
+        id: twostar,
+        value: "legums",
         averagerating: 3.9
-    },
-    {
-        id: jj - 1969,
-        name: "warp equalizer",
-        averagerating: 5.0
     }
+
 ];
